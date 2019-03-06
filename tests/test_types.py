d85cc3fd1521baf824c14ce2d3c4fa0a7a580af7
Peilonrayz <peilonrayz@gmail.com>
2019-03-06 21:38:49 +0000

    Test Travis is running on wanted versions (Py 3.5.0 and Py 3.6.0)

--- tests/test_types.py
+++ tests/test_types.py
@@ -8,3 +8,6 @@ import typing_inspect_lib
 class SpecialTestCase(TestCase):
     def test_union(self):
         self.assertEqual(typing_inspect_lib.Union, typing.Union)
+
+    def test_run_in_subversions(self):
+        self.assertEqual(typing.NoReturn, typing.NoReturn)
